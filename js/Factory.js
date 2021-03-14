afe643756895888d875ed8afb5f61831d77ca5b6
Yoann L'hostis <yolhostis@live.fr>
2021-03-14 23:37:01 +0100

    Début d'approche de la factory method et début d'intégration du json

--- js/Factory.js
+++ js/Factory.js
@@ -1,9 +1,33 @@
+const artists = [];
 
-class Factory {
-	constructor() {
-
+class PhotographerFactory {
+	constructor(city, country, id, name, portrait, price, tagline, tags) {
+		this.city = city;
+		this.country = country;
+		this.id = id;
+		this.name = name;
+		this.portrait = portrait;
+		this.price = price;
+		this.tagline = tagline;
+		this.tags = tags;
 	}
-	sayHello() {
-		alert("Hello");
+	askJsonAndCreatePhotographers() {
+		for(const photographer of data.photographers) {
+			// ou ? let id = 0;
+			let city = photographer.city;
+			let country = photographer.country;
+			let id = photographer.id;
+			//ou ? id++; Plus lisible que les ids proposés de base
+			let name = photographer.name;
+			let portrait = photographer.portrait;
+			let price = photographer.price;
+			let tagline = photographer.tagline;
+			const tags = photographer.tags;
+			const artist = new PhotographerFactory(city, country, id, name, portrait, price, tagline, tags);
+			artists.push(artist);
+		}
 	}
 };
+
+const photographersFactory = new PhotographerFactory();
+photographersFactory.askJsonAndCreatePhotographers();
--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 
-const ex = new Factory();
+console.log(artists);
 
-ex.sayHello();
+function displayPhotographers() { 
+	return document.getElementById("main").innerHTML =
+		artists.map(artist => {
+			return (
+				'<section class="main__photographer">' + 
+					'<div class="main__photographer__photo">' + artist.name + '</div>' + 
+					'<div class="main__photographer__description">' + artist.name + '</div>' + 
+					'<div class="main__photographer__tags">' + artist.name + '</div>' + 
+				'</section>'
+			)
+		});
+}
+
+displayPhotographers();
+
+
